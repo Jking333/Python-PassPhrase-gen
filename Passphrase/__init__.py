1350a57ab840573180d522d67ab9248ef7049aab
ose Okogbo <ose.okogbo@gmail.com>
2021-10-31 16:45:52 -0500

    Started building BaseModel for passphrase api

---
+++ Passphrase/__init__.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+import pydantic
+from typing import Optional
+import pprint
+
---
+++ Passphrase/passPhrase.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+import pydantic
+from typing import Optional
+import pprint
+
+
+class PassPhrase(pydantic.BaseModel):
+    passphrase_input : Optional[str]
+
+    @pydantic.validator('passphrase_input',allow_reuse=True)
+    def check_passphrase_valid(cls,passphrase_input:str):
+        print('started validator')
+        print(f'aye we did the test {passphrase_input}')
+        return passphrase_input
+    @staticmethod
+    def get_Passphrase_from_User():
+        user_input = input('Please Enter pass:\t')
+        return user_input
+
+
+def main():
+    pass_phrase_object = PassPhrase()
+    pass_phrase_object.passphrase_input=PassPhrase.get_Passphrase_from_User()
+    PassPhrase.check_passphrase_valid(pass_phrase_object.passphrase_input)
+    print(pass_phrase_object.__dict__)
+
+if '__name__' == main():
+    main()

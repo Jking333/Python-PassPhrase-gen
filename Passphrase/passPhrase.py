054ccaa480c9082d653321f8ee07cecf8e22e1f5
Promofinder2 <olivecaus@gmail.com>
2021-11-01 11:36:23 -0500

    Added character swap functionality in validator

--- Passphrase/passPhrase.py
+++ Passphrase/passPhrase.py
@@ -1,12 +1,11 @@
-import pandas as pd
-import numpy as np
 import pydantic
 from typing import Optional
 import pprint
+global char_repl_list
+char_repl_list = {'l':'1','L':'1','i':'1','!':'1','I':'1'}
 
 
 class PassPhrase(pydantic.BaseModel):
-    look_alike_dict = {1:['l','L','i','!','I']}
 
     
     passphrase_input : Optional[str]
@@ -14,9 +13,17 @@ class PassPhrase(pydantic.BaseModel):
     @pydantic.validator('passphrase_input',allow_reuse=True,pre=True,)
     def check_passphrase_valid(cls,passphrase_input:str):
         print('started validator\n')
-        passphrase_input = passphrase_input.lower()
-        print(f'aye we did the test\t\t {passphrase_input}\n')
-        return passphrase_input
+
+        passphrase_input_char_list = list(passphrase_input)
+        for index, character in enumerate(passphrase_input_char_list):
+            if character in char_repl_list.keys() and passphrase_input_char_list.count(character) == passphrase_input.count(character):
+                new_character = char_repl_list[character]
+                passphrase_input_char_list[index] = new_character
+                print('Character swapping')
+
+        new_passphrase = "".join(passphrase_input_char_list)
+        print(f'aye we did the test\t\tCHANGED-> {passphrase_input}\tTO-> {new_passphrase}')
+        return new_passphrase
     @staticmethod
     def get_Passphrase_from_User():
         user_input = input('Please Enter pass:\t')

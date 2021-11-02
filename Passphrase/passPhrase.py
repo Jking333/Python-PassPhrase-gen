d6162bfc3359ccfd8a9da965b0e65c6cbb4c41aa
ose Okogbo <ose.okogbo@gmail.com>
2021-11-01 19:39:47 -0500

    implemented function take in strings and convert phrase to acronym

--- Passphrase/passPhrase.py
+++ Passphrase/passPhrase.py
@@ -1,8 +1,11 @@
 import pydantic
 from typing import Optional
 import pprint
+import regex as re
 global char_repl_list
-char_repl_list = {'l':'1','L':'1','i':'1','!':'1','I':'1'}
+char_repl_list = {'l':'1','L':'1','i':'1','!':'1','I':'1', }
+global new_list
+new_list = []
 
 
 class PassPhrase(pydantic.BaseModel):
@@ -14,14 +17,27 @@ class PassPhrase(pydantic.BaseModel):
     def check_passphrase_valid(cls,passphrase_input:str):
         print('started validator\n')
 
-        passphrase_input_char_list = list(passphrase_input)
-        for index, character in enumerate(passphrase_input_char_list):
-            if character in char_repl_list.keys() and passphrase_input_char_list.count(character) == passphrase_input.count(character):
-                new_character = char_repl_list[character]
-                passphrase_input_char_list[index] = new_character
-                print('Character swapping')
+        passphrase_input_char_list = passphrase_input.split(" ")
+        for index, word in enumerate(passphrase_input_char_list):
+            if index == 2:
+                try:
+                        
+                    new_word = list(word)
+                    new_word[4] = new_word[4].upper()
+                    edited_word = "".join(new_word) 
 
-        new_passphrase = "".join(passphrase_input_char_list)
+                    new_list.append(edited_word)
+                except IndexError:
+                    new_list.append(word.upper())
+            else:
+                new_list.append(word[0])
+           
+        for index, character in enumerate(new_list):
+            if character in char_repl_list.keys():
+                new_character = char_repl_list[character]
+                new_list[index] = new_character
+                print('character swapping')
+        new_passphrase = "".join(new_list)
         print(f'aye we did the test\t\tCHANGED-> {passphrase_input}\tTO-> {new_passphrase}')
         return new_passphrase
     @staticmethod
